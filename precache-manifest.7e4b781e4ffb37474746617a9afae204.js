bfb5438c8fe1fbab958d2a53f4efbb440e6e1d73
Greyti <47736158+Greyti8888@users.noreply.github.com>
2019-10-29 13:53:57 +0200

    Updates

---
+++ precache-manifest.7e4b781e4ffb37474746617a9afae204.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3d866a5e66ed116ef66d0e972ca723",
+    "url": "/Greyti8888/pokedex/index.html"
+  },
+  {
+    "revision": "8eb6e62dfa6198646086",
+    "url": "/Greyti8888/pokedex/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c6ebc5c8362c73535ebb",
+    "url": "/Greyti8888/pokedex/static/js/2.b9c9e324.chunk.js"
+  },
+  {
+    "revision": "8eb6e62dfa6198646086",
+    "url": "/Greyti8888/pokedex/static/js/main.669ebaa3.chunk.js"
+  },
+  {
+    "revision": "491aca8e00f94c8cfe58",
+    "url": "/Greyti8888/pokedex/static/js/runtime-main.e870f935.js"
+  }
+]);
